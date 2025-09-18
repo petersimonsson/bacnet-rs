fcd7dbb65134296d0a2c5d5c60f439af3c5ebb07
Tom van Hest <tom.van.hest@nordomatic.com>
2025-09-18 18:52:09 +0300

    Fix remaining CI issues
    - Fix cargo dependencies for proper std/no-std support
    - Add chrono clock feature for time functionality
    - Remove crc/std feature that doesn't exist
    - Fix duplicate extern crate alloc declaration
    - Update CI to test only supported feature combinations
    - Remove unsupported no-std configurations from CI

--- src/lib.rs
+++ src/lib.rs
@@ -137,11 +137,6 @@ pub use object::{BacnetObject, ObjectType, PropertyIdentifier};
 pub use service::{ConfirmedServiceChoice, ServiceError, UnconfirmedServiceChoice};
 pub use vendor::{format_vendor_display, get_vendor_info, get_vendor_name, VendorInfo};
 
-#[cfg(feature = "std")]
-extern crate std;
-
-#[cfg(not(feature = "std"))]
-extern crate alloc;
 
 /// BACnet protocol version as defined in ASHRAE 135
 pub const BACNET_PROTOCOL_VERSION: u8 = 1;
